b5513b45ec421c9ef10e502767896232ce09ef72
Sanjan Gadde <sg8147@nyu.edu>
2025-08-28 17:33:13 -0400

    stage13 project

--- project/src/utils.py
+++ project/src/utils.py
@@ -2,3 +2,9 @@ import pandas as pd
 
 def get_summary_stats(df):
     return df.describe()
+
+def predict(model, features, round_prediction=False):
+    pred = model.predict([features])[0]
+    if round_prediction:
+        pred = round(pred, 2)
+    return pred
